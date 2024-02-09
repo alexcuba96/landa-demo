8b7959531bc9d8fd978bdb3dd21b5b7de0db2502
Alexander Chalarca Caro <59976584+alexcuba96@users.noreply.github.com>
2024-02-09 07:57:00 -0500

    Add files via upload
    First commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <ContactForm />
+</template>
+
+<script>
+import ContactForm from './components/ContactForm.vue'
+
+export default {
+  name: 'App',
+  components: {
+    ContactForm
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
